90bbb6795963671fd3ab9ee46ec18cf0656df900
Santiago Cézar <santiagocezar2013@gmail.com>
2024-11-30 14:47:13 -0300

    it's been a while

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import unocssPlugin from '@unocss/vite';
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     plugins: [unocssPlugin()]
   }
